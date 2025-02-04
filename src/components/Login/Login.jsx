8ce804ed3cc41d7ea337228effa16d7e119dce71
emonShimoul <emonshimoul@gmail.com>
2025-02-04 15:49:48 +0600

    firebase setup and use google sign in

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,22 @@
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import auth from "../../firebase/firebase.init";
+
 const Login = () => {
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .cathc((error) => {
+        console.log("Error: ", error);
+      });
+  };
+
   return (
     <div>
-      <button>Login with Google</button>
+      <button onClick={handleGoogleSignIn}>Login with Google</button>
     </div>
   );
 };
