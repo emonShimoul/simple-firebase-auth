97a432667a18f03ea9ecd5d517d1d84549eb903f
emonShimoul <emonshimoul@gmail.com>
2025-02-04 17:21:57 +0600

    display user info and sign out

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,49 @@
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 import auth from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState(null);
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        console.log(result);
+        console.log(result.user);
+        setUser(result.user);
       })
-      .cathc((error) => {
+      .catch((error) => {
         console.log("Error: ", error);
+        setUser(null);
+      });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Sign-out successful");
+        setUser(null);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
       });
   };
 
   return (
     <div>
-      <button onClick={handleGoogleSignIn}>Login with Google</button>
+      {user ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Login with Google</button>
+      )}
+      {user && (
+        <div>
+          <h4>{user.displayName}</h4>
+          <p>{user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 };
