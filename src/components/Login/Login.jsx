5f047b8e66e4dac6025c69ef7e8633b8afcf509b
emonShimoul <emonshimoul@gmail.com>
2025-02-07 01:43:35 +0600

    add a login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -12,6 +12,13 @@ const Login = () => {
   const provider = new GoogleAuthProvider();
   const githubProvider = new GithubAuthProvider();
 
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+  };
+
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
@@ -50,14 +57,67 @@ const Login = () => {
 
   return (
     <div>
-      <h2 className="text-4xl">Login</h2>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
       {user ? (
-        <button onClick={handleSignOut}>Sign Out</button>
+        <button className="btn btn-secondary" onClick={handleSignOut}>
+          Sign Out
+        </button>
       ) : (
-        <>
-          <button onClick={handleGoogleSignIn}>Login with Google</button>
-          <button onClick={handleGithubSignIn}>Login with Github</button>
-        </>
+        <div className="my-4 flex gap-4 justify-center">
+          <button className="btn btn-success" onClick={handleGoogleSignIn}>
+            Login with Google
+          </button>
+          <button className="btn btn-info" onClick={handleGithubSignIn}>
+            Login with Github
+          </button>
+        </div>
       )}
       {user && (
         <div>
